a71554e5b57542f89a5d6809852c8be2e37fb52b
emanuelbatista <emanuelbatista2011@gmail.com>
2016-03-28 22:50:44 -0300

    adicionando diagramas

--- pushing-servidor/src/main/java/br/edu/ifpb/pos/pushing/servidor/repository/ResponseRepository.java
+++ pushing-servidor/src/main/java/br/edu/ifpb/pos/pushing/servidor/repository/ResponseRepository.java
@@ -5,7 +5,6 @@
  */
 package br.edu.ifpb.pos.pushing.servidor.repository;
 
-
 import br.edu.ifpb.pos.pushing.core.Response;
 import br.edu.ifpb.pos.pushing.core.ResponseType;
 import java.util.HashMap;
@@ -16,32 +15,50 @@ import java.util.Map;
  * @author Emanuel Batista da Silva Filho - https://github.com/emanuelbatista
  */
 public class ResponseRepository {
-    
-    private final Map<String,Response> responses;
-    
+
+    private final Map<String, Response> responses;
+    private final IndentificationManager indentificationManager;
+
     private ResponseRepository() {
-        this.responses=new HashMap();
+        this.responses = new HashMap();
+        this.indentificationManager = IndentificationManager.getInstance();
     }
-    
+
     public static ResponseRepository getInstance() {
         return ResponseRepositoryHolder.INSTANCE;
     }
-    
+
     private static class ResponseRepositoryHolder {
 
         private static final ResponseRepository INSTANCE = new ResponseRepository();
     }
-    
-    public void store(String id,Response response){
+
+    public void store(String id, Response response) {
         responses.put(id, response);
     }
-    
-    public Response getResponse(String uid){
-        Response response=responses.get(uid);
-        if(response==null){
+
+    public Response getResponse(String uid) {
+        Response response = responses.get(uid);
+        if (response == null) {
             return new Response(ResponseType.NO_RESPONSE, null);
         }
-        responses.remove(uid);
+        new RemoveResources(uid).start();
         return response;
+    }
+
+    private class RemoveResources extends Thread {
+
+        private final String uid;
+
+        public RemoveResources(String uid) {
+            this.uid = uid;
+        }
+
+        @Override
+        public void run() {
+            responses.remove(uid);
+            indentificationManager.release(uid);
+        }
+
     }
 }
